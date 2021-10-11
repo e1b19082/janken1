a55fb8399c6c3ef0058c8246e195c599cf347299
e1b19082 <itpassport.kh@gmail.com>
2021-10-11 19:02:16 +0900

    じゃんけんの手を選んでCPUと対戦する処理を実装

--- src/main/java/oit/is/z0209/kaizi/janken1/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0209/kaizi/janken1/controller/Lec02Controller.java
@@ -19,4 +19,28 @@ public class Lec02Controller {
     model.addAttribute("name", name);
     return "lec02.html";
   }
+
+  @GetMapping("/lec02janken")
+  public String sample23(@RequestParam String hand, ModelMap model) {
+    String computerHand = "Gu";
+    String jankenResult = "";
+
+    if (hand.equals("Gu") == computerHand.equals("Gu")) {
+      jankenResult = "Draw!";
+    } else if (hand.equals("Choki") == computerHand.equals("Gu")) {
+      jankenResult = "You Lose!";
+    } else if (hand.equals("Pa") == computerHand.equals("Gu")) {
+      jankenResult = "You Win!";
+    }
+
+    model.addAttribute("sentence1", "あなたの手 ");
+    model.addAttribute("sentence2", "相手の手 ");
+    model.addAttribute("sentence3", "結果 ");
+    model.addAttribute("hand", hand);
+    model.addAttribute("computerHand", computerHand);
+    model.addAttribute("jankenResult", jankenResult);
+
+    return "lec02.html";
+
+  }
 }
