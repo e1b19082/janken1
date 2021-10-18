fc42461b00a699f28910dcd830651c2d3b93b275
e1b19082 <itpassport.kh@gmail.com>
2021-10-18 12:21:15 +0900

    ログイン後，ログインユーザ名，エントリー(ログイン)しているすべてのユーザを表示するように変更

--- src/main/java/oit/is/z0209/kaizi/janken1/controller/Lec02Controller.java
+++ src/main/java/oit/is/z0209/kaizi/janken1/controller/Lec02Controller.java
@@ -1,16 +1,30 @@
 package oit.is.z0209.kaizi.janken1.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import oit.is.z0209.kaizi.janken1.model.Entry;
 
 @Controller
 public class Lec02Controller {
+
+  @Autowired
+  private Entry room;
+
   @GetMapping("/lec02")
-  public String lec02() {
+  public String lec02(Principal prin, ModelMap model) {
+
+    String loginUser = prin.getName();
+    this.room.addUser(loginUser);
+    model.addAttribute("room", this.room);
+
     return "/lec02.html";
   }
 
